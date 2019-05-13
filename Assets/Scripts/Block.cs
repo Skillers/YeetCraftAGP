9532d5446eb40dc7b3974d87c58a3ea90618dcd1
Rick <Rick.nl@hotmail.com>
2019-05-13 21:49:08 +0200

    Texture Fix

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -31,14 +31,14 @@ public class Block {
                                 new Vector2( 0.125f, 0.8125f ),new Vector2( 0.1875f, 0.8125f )},		
         /*NOCRACK*/			{new Vector2( 0.6875f, 0f ), new Vector2( 0.75f, 0f),
                                 new Vector2( 0.6875f, 0.0625f ),new Vector2( 0.75f, 0.0625f )},
-		/*CRACK1*/			{ new Vector2(0f,0f),  new Vector2(0.0625f,0f),
-                                 new Vector2(0f,0.0625f), new Vector2(0.0625f,0.0625f)},
- 		/*CRACK2*/			{ new Vector2(0.0625f,0f),  new Vector2(0.125f,0f),
-                                 new Vector2(0.0625f,0.0625f), new Vector2(0.125f,0.0625f)},
- 		/*CRACK3*/			{ new Vector2(0.125f,0f),  new Vector2(0.1875f,0f),
-                                 new Vector2(0.125f,0.0625f), new Vector2(0.1875f,0.0625f)},
- 		/*CRACK4*/			{ new Vector2(0.1875f,0f),  new Vector2(0.25f,0f),
-                                 new Vector2(0.1875f,0.0625f), new Vector2(0.25f,0.0625f)}
+		/*CRACK1*/			{ new Vector2(0f,0f),  new Vector2(0.0605f,0f),
+                                 new Vector2(0f,0.0625f), new Vector2(0.0605f,0.0625f)},
+ 		/*CRACK2*/			{ new Vector2(0.0625f,0f),  new Vector2(0.123f,0f),
+                                 new Vector2(0.0625f,0.0625f), new Vector2(0.123f,0.0625f)},
+ 		/*CRACK3*/			{ new Vector2(0.125f,0f),  new Vector2(0.1855f,0f),
+                                 new Vector2(0.125f,0.0625f), new Vector2(0.1855f,0.0625f)},
+ 		/*CRACK4*/			{ new Vector2(0.1875f,0f),  new Vector2(0.248f,0f),
+                                 new Vector2(0.1875f,0.0625f), new Vector2(0.248f,0.0625f)}
                         };
 
     public Block(BlockType b, Vector3 pos, GameObject p, Chunk o)
